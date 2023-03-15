65a10663f2a5bc75a06069bd04f6a74ed745abc7
mtrh93 <mtrh.younger@gmail.com>
2023-03-15 13:43:35 +0000

    add further validation

--- run.py
+++ run.py
@@ -16,14 +16,23 @@ def get_occupancy_data():
     """
     Get occupancy figures from the user
     """
-    print("Please enter occupancy data from the last day.")
-    print("The figures entered should be 5 numbers, seperated by comas.")
-    print("Wxample: 10,20,30,40,50")
 
-    occupancy_str = input("Enter the ocupancy data here: ")
+    while True:
+        
+        print("Please enter occupancy data from the last day.")
+        print("The figures entered should be 5 numbers, seperated by comas.")
+        print("Example: 10,20,30,40,50")
+
+        occupancy_str = input("Enter the ocupancy data here: ")
     
-    occupancy_data = occupancy_str.split(",")
-    vailidate_data(occupancy_data)
+        occupancy_data = occupancy_str.split(",")
+        vailidate_data(occupancy_data)
+
+        if vailidate_data(occupancy_data):
+            print("Occupancy Data is valid")
+            break
+
+    return occupancy_data
 
 def vailidate_data(values):
     """
@@ -32,11 +41,15 @@ def vailidate_data(values):
     and if it cant be converted referring it back to ask to re-enter
     """
     try:
+        [int(value) for value in values]
         if len(values) != 5:
             raise ValueError(
                 f"Exactly 5 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid occupancy data: {e}, please try again.\n")
+        return False
+    
+    return True
 
-get_occupancy_data()
+data = get_occupancy_data()
